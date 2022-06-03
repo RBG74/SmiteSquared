cdc4955d5251929fe23e940bfc81dfe5054d0acc
RBG <remy.bernardgranger@gmail.com>
2022-06-03 11:42:47 +0200

    Reset angular project

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HttpClientModule
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
